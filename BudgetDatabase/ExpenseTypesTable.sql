4a9e70ac86952901a70f7e3a5fb76ffcb10c93e9
Tshiamo <tshiamo.mahloko@vumatel.co.za>
2022-02-18 14:05:59 +0200

    Added the expense table and expense type table to createDatabase file.

--- BudgetDatabase/ExpenseTypesTable.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE ExpenseTypes(
-    ExpenseTypeID INT PRIMARY KEY NOT NULL IDENTITY(1,1),
-    ExpenseTypeName VARCHAR(200) NOT NULL,
-    ExpenseDescription VARCHAR(250) NOT NULL
-);
-
-GO
