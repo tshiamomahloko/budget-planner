ad025026c366393656eecf6a23b6b1496ce1d6de
Sean Groenewald <seang@bbd.co.za>
2022-02-23 13:42:12 +0200

    Improved uspNewBudgetCopy by removing dynamic SQL and changing budgetId parameter to int. Updated test script to reflect new changes.

--- test_uspNewBudgetCopy.sql
+++ test_uspNewBudgetCopy.sql
@@ -2,6 +2,6 @@ USE BudgetDatabase
 
 GO
 
-EXEC uspNewBudgetCopy @budgetId = '1',
+EXEC uspNewBudgetCopy @budgetId = 1,
 	@startDate = '20000101',
 	@endDate ='20001230'
--- uspNewBudgetCopy.sql
+++ uspNewBudgetCopy.sql
@@ -6,25 +6,26 @@ IF OBJECT_ID ( 'uspNewBudgetCopy', 'P' ) IS NOT NULL
 GO
 
 CREATE PROCEDURE uspNewBudgetCopy
-	@budgetId varchar(20),
+	@budgetId int,
 	@startDate date,
 	@endDate date
 AS
 BEGIN
 	SET NOCOUNT ON;
-	DECLARE @sql NVARCHAR(2048)
-	DECLARE @newBudgetId nvarchar(20)
+	DECLARE @newBudgetId int
 	
 	-- Create new Budget
-	SET @sql = FORMATMESSAGE('INSERT INTO [Budget] (CustomerID, StartDate, EndDate) VALUES ((SELECT TOP 1 [CustomerID] FROM [Budget] WHERE [BudgetID] = %s), ''%s'', ''%s''); SELECT @budgetId = SCOPE_IDENTITY();', @budgetId, CONVERT(varchar(12), @startDate, 1), CONVERT(varchar(12), @endDate, 1));
-	EXEC sp_executesql @sql, N'@budgetId int out', @newBudgetId out;
+	INSERT INTO [Budget] (CustomerID, StartDate, EndDate) VALUES ((SELECT TOP 1 [CustomerID] FROM [Budget] WHERE [BudgetID] = @budgetId), @startDate, @endDate); 
+	
+	-- Get ID of new Budget
+	SET @newBudgetId = SCOPE_IDENTITY();
 
 	-- Add expenses
-	SET @sql = FORMATMESSAGE('INSERT INTO [ExpenseBudget] SELECT %s AS [BudgetID], [ExpenseID], [ExpenseAmount] FROM [ExpenseBudget] WHERE BudgetID = %s', @newBudgetId, @budgetId);
-	EXEC sp_executesql @sql;
+	INSERT INTO [ExpenseBudget] SELECT @newBudgetId AS [BudgetID], [ExpenseID], [ExpenseAmount] FROM [ExpenseBudget] WHERE BudgetID = @budgetId
 
 	-- Add incomes
-	SET @sql = FORMATMESSAGE('INSERT INTO [IncomeBudget] SELECT %s AS [BudgetID], [IncomeID], [IncomeAmount] FROM [IncomeBudget] WHERE BudgetID = %s', @newBudgetId, @budgetId);
-	EXEC sp_executesql @sql;
+	INSERT INTO [IncomeBudget] SELECT @newBudgetId AS [BudgetID], [IncomeID], [IncomeAmount] FROM [IncomeBudget] WHERE BudgetID = @budgetId
+
+	PRINT FORMATMESSAGE('Successfully created new budget with id: %d', @newBudgetId);
 END
 GO
