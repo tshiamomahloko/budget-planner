d2a0b5135fb1be2625248ff4a2aa5381dff40755
DmhltnFisFinstbttn <98146199+DmhltnFisFinstbttn@users.noreply.github.com>
2022-02-24 09:19:33 +0200

    Update viewQueries.sql
    Clean up comments.

--- viewQueries.sql
+++ viewQueries.sql
@@ -1,3 +1,6 @@
+USE BudgetDatabase;
+Go
+
 /*
 View that returns all the possible income types
 a customer can choose from and their descriptions.
@@ -21,13 +24,8 @@ FROM ExpenseType;
 -- SELECT * FROM ExpenseTypeView;
 
 /*
-Possible other idea for a view:
 View that returns all the customers in the database without
 displaying their passwords.
-This view can be used by an administrator to view the customers
-in the database without risking them having access to the
-customer's password. It is possible for an admin to delete
-a customer record through the view.
 */
 
 CREATE VIEW [customerView] AS
@@ -36,28 +34,3 @@ FROM Customer;
 
 -- execute query:
 -- SELECT * FROM CustomerView;
-
-/*
-Can create a stored proc that will show a customer's
-inserted income and expense values. I forgot it can't be
-a view because you can't pass parameters to a view and
-you need the customerID param in order to do the SELECT
-query.
-
-Income Example:
-
-SELECT IncomeTypeName, IncomeName, IncomeAmount
-FROM IncomeType
-JOIN Income ON IncomeType.IncomeTypeID = Income.IncomeTypeID
-JOIN IncomeBudget ON Income.IncomeID = IncomeBudget.IncomeID
-WHERE Income.CustomerID = @customerID;
-
-Expense Example:
-
-SELECT ExpenseTypeName, ExpenseName, ExpenseAmount
-FROM ExpenseType
-JOIN Expense ON ExpenseType.ExpenseTypeID = Expense.ExpenseTypeID
-JOIN ExpenseBudget ON Expense.ExpenseID = ExpenseBudget.ExpenseID
-WHERE Expense.CustomerID = @customerID
-*/
-
